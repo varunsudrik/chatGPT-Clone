db9372613b111b6f4adf53b90bb0852440a78fc0
Varun Sudrik <varunsudrik99@gmail.com>
2023-04-14 07:20:01 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <section className="sidebar">
+        <button> + New Chat</button>
+        <ul className="history">
+          <li>Test history</li>
+        </ul>
+        <nav>
+          <p>Varun S</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1>VarunGPT</h1>
+        <ul className="feed"> </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+            <u>ChatGPT Mar 23 Version.</u> Free Research Preview. ChatGPT may
+            produce inaccurate information about people, places, or facts
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
